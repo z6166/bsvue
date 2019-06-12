474c2789c5ede37ec849dfd6856d39df352842bd
zhangshijie <3160102448@zju.edu.cn>
2019-06-12 19:50:21 +0800

    修复bug

---
+++ src/utils.js
@@ -0,0 +1,25 @@
+export default{
+    install(Vue,options)
+    {
+        Vue.prototype.logout = function () {
+            this.$cookies.remove('token');
+            this.$cookies.remove('username');
+            this.$cookies.remove('uid');
+            this.$router.go(0);
+        };
+        Vue.prototype.getLocalTime = function (timestamp) {
+            var d = new Date(timestamp);
+            return (d.getFullYear()) + "-" +
+                (d.getMonth() + 1) + "-" +
+                (d.getDate()) + " " +
+                (d.getHours()) + ":" +
+                (d.getMinutes()) + ":" +
+                (d.getSeconds());
+        };
+        Vue.prototype.accessKey = 'c_z_GCgmtsRNcIZ3fHTY-1rv2Fwm4ZuRclHv3Nhs';
+        Vue.prototype.secretKey = 'SUWR-z0HCx3ZOF2dPWbSP74LeLXP9e-GsHMvrfDY';
+        Vue.prototype.bucket = "mypic";
+        Vue.prototype.baseurl = "http://3.113.9.204:8000";
+        Vue.prototype.publicBucketDomain = "psx59ycao.bkt.clouddn.com";
+    }
+}
